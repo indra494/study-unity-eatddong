aeae7f18bf86b7f6d0e8450d10d8b9f85764341c
indra494 <52900183+indra494@users.noreply.github.com>
2023-10-19 15:37:15 +0900

    first commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject rain;
+    public GameObject panel;
+    public static GameManager I;
+    public Text scoreText;
+    public Text timeText;
+    int totalScore;
+    float limit = 60f;
+
+    private void Awake()
+    {
+        I = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeRain", 0, 0.5f);
+        initGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        limit -= Time.deltaTime;
+        if(limit < 0)
+        {
+            Time.timeScale = 0.0f;
+            panel.SetActive(true);
+            limit = 0.0f;
+        }
+
+        timeText.text = limit.ToString("N2");
+    }
+
+    void makeRain()
+    {
+        Instantiate(rain);
+    }
+
+    public void addScore(int score)
+    {
+        totalScore += score;
+        scoreText.text = totalScore.ToString();
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void initGame()
+    {
+        Time.timeScale = 1.0f;
+        totalScore = 0;
+        limit = 30f;
+    }
+
+}
